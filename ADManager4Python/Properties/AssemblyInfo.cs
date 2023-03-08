8ed4757fe455431ddd8831ef264719231bbf4d0d
Vadim Katsman <vadimk@sctalt.com>
2023-03-08 16:40:46 -0600

    Releasable version of AppDomainManager

--- ADManager4Python/Properties/AssemblyInfo.cs
+++ ADManager4Python/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ADManager4Python")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("AppDomain Manager for Python-based hosts")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Vadim Katsman")]
+[assembly: AssemblyCompany("SCT Alternative Inc. - RD division - Vadim Katsman")]
 [assembly: AssemblyProduct("AppDomain Manager For Python")]
-[assembly: AssemblyCopyright("Copyright © Vadim Katsman 2023")]
+[assembly: AssemblyCopyright("Copyright © SCT Alternative Inc. and Vadim Katsman 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("100.2023.0306.0")]
-[assembly: AssemblyFileVersion("100.2023.0306.0")]
+[assembly: AssemblyVersion("100.2023.0308.0")]
+[assembly: AssemblyFileVersion("100.2023.0308.0")]
